9bb8309f60cf80414a90034b794a6bb464ce5a9c
Bell <46092446+BellKornel@users.noreply.github.com>
2018-12-27 19:17:06 +0900

    Update foto_action.php

--- foto_action.php
+++ foto_action.php
@@ -3,7 +3,7 @@ $link = mysqli_connect('0.0.0.0', 'anton', '', 'DB')
 	or die('Error: ' . mysqli_connect_error());
 $one = mysqli_real_escape_string($link, $_POST['foto']['name']);
 $two = mysqli_real_escape_string($link, $_POST['apartment']);
-echo $one;
+echo "<P>".$one."/P";
 echo "<br>";
 //$id = 0;
 //$SQLresult = mysqli_query($link, 'select id from foto');
