43577cf27344dc10e40e1f19b30fdb2fe4c88434
Bell <46092446+BellKornel@users.noreply.github.com>
2018-12-28 01:43:41 +0900

    Update foto_action.php

--- foto_action.php
+++ foto_action.php
@@ -2,14 +2,6 @@
 $link = mysqli_connect('0.0.0.0', 'anton', '', 'DB')
 	or die('Error: ' . mysqli_connect_error());
 
-$path="/var/www/html/file/";
-if (!@copy($_FILES['photo']['tmp_name'], $path . $_FILES['photo']['name']))
-echo "<P>Error on file loading]</P>";
-else
-echo "<P>Photo uploaded succesfully</P>";
-
-$one = mysqli_real_escape_string($link, $_FILES['photo']['name']);
-$two = mysqli_real_escape_string($link, $_POST['apartment']);
 $id = 0;
 $SQLresult = mysqli_query($link, 'select id from foto');
 while($result = mysqli_fetch_array($SQLresult, MYSQLI_NUM))
@@ -17,6 +9,14 @@ while($result = mysqli_fetch_array($SQLresult, MYSQLI_NUM))
 	if($result[0] > $id) {$id = $result[0];}
 }
 $id = $id+1;
+$path="/var/www/html/file/";
+if (move_uploaded_file($_FILES['photo']['tmp_name'], $path . $id))
+echo "<P>Photo uploaded succesfully</P>";
+else
+echo "<P>Error on file loading</P>";
+
+$one = mysqli_real_escape_string($link, $_FILES['photo']['name']);
+$two = mysqli_real_escape_string($link, $_POST['apartment']);
 $SQLquery = 'insert into foto values(' . $id . ', "' . $one . '", ' . $two . ')';
 if (mysqli_query($link, $SQLquery))
 {
