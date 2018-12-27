2af957d20f8525a08a65afb626c553f728eba6ad
Bell <46092446+BellKornel@users.noreply.github.com>
2018-12-27 17:10:08 +0900

    Update apartment.php

--- apartment.php
+++ apartment.php
@@ -31,5 +31,40 @@
 		mysqli_close($link);
 	?>
 		<a href="index.html"> <P>GO BACK</P> </a>
+		  <TD>
+			  <P>Add new apartment:</P>
+			  <form action="apartment_action.php" method="post">
+          		  	Address: <input type="text" name="address">
+          		  	<br>
+          		  	Area: <input type="text" name="area">
+          		  	<br>
+          		  	Rooms: <input type="text" name="rooms">
+          		  	<br>
+          		  	Floor: <input type="text" name="floor">
+          		  	<br>
+          		  	Description: <input type="text" name="description">
+          		  	<br>
+          		  	Commision: <input type="text" name="commsision">
+          		  	<br>
+				Owner: 
+					<select name="owner">
+					<?php 
+					$link = mysqli_connect('0.0.0.0', 'anton', '', 'DB')					
+	    					or die('Error: ' . mysqli_connect_error());
+						
+					$SQLquery = 'SELECT id, CONCAT(name, \' \', family) from human';
+					$SQLresult = mysqli_query($link,$SQLquery);
+					while ($result = mysqli_fetch_array($SQLresult,MYSQLI_NUM))
+					{
+						printf('<option value=%d>%s</option>',$result[0],$result[1]);
+					}
+					mysqli_free_result($SQLresult);
+					mysqli_close($link);
+					?>
+					</select>
+				  <br>
+            		  	<input type="submit" value="Add apartment">
+      			  </form>
+		</TD>
 	</body>
 </html>
