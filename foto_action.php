4d9ba99e80d43d7738c72eb95bf1f9bb4aaa6751
Bell <46092446+BellKornel@users.noreply.github.com>
2018-12-28 08:23:23 +0900

    Update foto_action.php

--- foto_action.php
+++ foto_action.php
@@ -3,21 +3,22 @@ $link = mysqli_connect('0.0.0.0', 'anton', '', 'DB')
 	or die('Error: ' . mysqli_connect_error());
 
 $file = $_FILES['photo'];
+$filename = time()
 $target_dir = "file/";
-$target_file = $target_dir . $file["name"];
+$target_file = $target_dir . $filename;
 
  if ( !file_exists($target_dir) ) {
      mkdir ($target_dir, 0744);
 echo "dir doesn't exist, trying to create dir!";
  }
 
-if (move_uploaded_file($file['tmp_name'], $target_dir))
+if (move_uploaded_file($filename, $target_dir))
 {
 echo "<P>Photo uploaded succesfully</P>";
 }
 else
 {
-echo "tmp_name: " . $file["tmp_name"] . "<br>name: " . $file["name"] . "<br>folder with file name: " . $target_file . "<br>";
+echo "tmp_name: " . $file["tmp_name"] . "<br>name: " . $filename . "<br>folder with file name: " . $target_file . "<br>";
 echo "<P>Error on file loading: " . $file["error"] . "</P>";
 }
 
