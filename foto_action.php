34c183532e931ae990a57e35e7542e396e7aba9a
Bell <46092446+BellKornel@users.noreply.github.com>
2018-12-27 19:20:38 +0900

    Update foto_action.php

--- foto_action.php
+++ foto_action.php
@@ -1,7 +1,7 @@
 <?php
 $link = mysqli_connect('0.0.0.0', 'anton', '', 'DB')
 	or die('Error: ' . mysqli_connect_error());
-$one = mysqli_real_escape_string($link, $_POST['photo']['tmp_name']);
+$one = mysqli_real_escape_string($link, $_FILES['photo']['name']);
 $two = mysqli_real_escape_string($link, $_POST['apartment']);
 echo $one;
 echo "<br>";
