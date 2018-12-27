02176c675b151b31f353b2edc46e8a93758bc1a5
Bell <46092446+BellKornel@users.noreply.github.com>
2018-12-27 16:54:59 +0900

    Create foto_action.php

---
+++ foto_action.php
@@ -0,0 +1,26 @@
+<?php
+$link = mysqli_connect('0.0.0.0', 'anton', '', 'DB')
+	or die('Error: ' . mysqli_connect_error());
+$one = mysqli_real_escape_string($link, $_POST['Foto']);
+$two = mysqli_real_escape_string($link, $_POST['Apartment']);
+echo $one;
+echo $two;
+$id = 0;
+$SQLresult = mysqli_query($link, 'select id from foto');
+while($result = mysqli_fetch_array($SQLresult, MYSQLI_NUM))
+{
+	if($result[0] > $id) {$id = $result[0];}
+}
+$id = $id+1;
+//$SQLquery = 'insert into foto values("' . $id . '", "' . $one . '", "' . $two . '")';
+if (mysqli_query($link, $SQLquery))
+{
+	echo "<BR>New record created succesfully!";
+}
+else
+{
+	echo "<BR>Error: " . $sql . "<BR>" . mysqli_error($link);
+}
+mysqli_close($link);
+printf('<a href="foto.php"><P>Go back</P></a>');
+?>
