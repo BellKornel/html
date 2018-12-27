0e75ce69f418ce17e889b02c247e20642dfd7dcb
Bell <46092446+BellKornel@users.noreply.github.com>
2018-12-27 16:59:47 +0900

    Update foto_action.php

--- foto_action.php
+++ foto_action.php
@@ -6,22 +6,22 @@ $two = mysqli_real_escape_string($link, $_POST['apartment']);
 echo $one;
 echo '<BR>';
 echo $two;
-//$id = 0;
-//$SQLresult = mysqli_query($link, 'select id from foto');
-//while($result = mysqli_fetch_array($SQLresult, MYSQLI_NUM))
-//{
-//	if($result[0] > $id) {$id = $result[0];}
-//}
-//$id = $id+1;
-//$SQLquery = 'insert into foto values("' . $id . '", "' . $one . '", "' . $two . '")';
-//if (mysqli_query($link, $SQLquery))
-//{
-//	echo "<BR>New record created succesfully!";
-//}
-//else
-//{
-//	echo "<BR>Error: " . $sql . "<BR>" . mysqli_error($link);
-//}
-//mysqli_close($link);
+$id = 0;
+$SQLresult = mysqli_query($link, 'select id from foto');
+while($result = mysqli_fetch_array($SQLresult, MYSQLI_NUM))
+{
+	if($result[0] > $id) {$id = $result[0];}
+}
+$id = $id+1;
+$SQLquery = 'insert into foto values("' . $id . '", "' . $one . '", "' . $two . '")';
+if (mysqli_query($link, $SQLquery))
+{
+	echo "<BR>New record created succesfully!";
+}
+else
+{
+	echo "<BR>Error: " . $sql . "<BR>" . mysqli_error($link);
+}
+mysqli_close($link);
 printf('<a href="foto.php"><P>Go back</P></a>');
 ?>
