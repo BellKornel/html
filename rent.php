9bf24ad7a997dd2169c647f76e8129d63708e239
Bell <46092446+BellKornel@users.noreply.github.com>
2018-12-27 18:39:22 +0900

    Update rent.php

--- rent.php
+++ rent.php
@@ -31,5 +31,38 @@
 		mysqli_close($link);
 	?>
 		<a href="index.html"> <P>GO BACK</P> </a>
+	 <TD>
+			  <P>Add new foto:</P>
+			  <form action="rent_action.php" method="post">
+				Ownert: 
+					<select name="owner">
+					<?php 
+					$link = mysqli_connect('0.0.0.0', 'anton', '', 'DB')					
+	    					or die('Error: ' . mysqli_connect_error());
+						
+					$SQLquery = 'SELECT id, CONCAT(name, \' \', family) from human';
+					$SQLresult = mysqli_query($link,$SQLquery);
+					while ($result = mysqli_fetch_array($SQLresult,MYSQLI_NUM))
+					{
+						printf('<option value=%d>%s</option>',$result[0],$result[1]);
+					}
+					mysqli_free_result($SQLresult);
+					mysqli_close($link);
+					?>
+					</select>
+				<br>
+          		  	Apartment: <input type="text" name="apartment">
+          		  	<br>
+          		  	Payment per months: <input type="text" name="ppm">
+          		  	<br>
+          		  	Prepayment: <input type="text" name="prepayment">
+          		  	<br>
+          		  	Rent begin: <input type="text" name="begin">
+          		  	<br>
+          		  	Rent end: <input type="text" name="end">
+          		  	<br>
+            		  	<input type="submit" value="Add rent">
+      			  </form>
+	</TD>
 	</body>
 </html>
