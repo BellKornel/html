d5d5b26d49126cdf3c4d664211f562d6902a7d7f
Bell <46092446+BellKornel@users.noreply.github.com>
2018-12-29 16:13:46 +0900

    Update foto_action.php

--- foto_action.php
+++ foto_action.php
@@ -7,20 +7,20 @@ $fliename = date('Y_m_d_H_i_s');
 $target_dir = "file/";
 $target_file = $target_dir . $filename;
 
- //if ( !file_exists($target_dir) ) {
-//     mkdir ($target_dir, 0744);
-//echo "dir doesn't exist, trying to create dir!";
-//}
+if ( !file_exists($target_dir) ) {
+     mkdir ($target_dir, 0744);
+echo "dir doesn't exist, trying to create dir!";
+}
 
-//if (move_uploaded_file($filename, $target_file))
-//{
-//echo "<P>Photo uploaded succesfully</P>";
-//}
-//else
-//{
-//echo "tmp_name: " . $file["tmp_name"] . "<br>name: " . $filename . "<br>folder with file name: " . $target_file . "<br>";
-//echo "<P>Error on file loading: " . $file["error"] . "</P>";
-//}
+if (move_uploaded_file($filename, $target_file))
+{
+echo "<P>Photo uploaded succesfully</P>";
+}
+else
+{
+echo "tmp_name: " . $file["tmp_name"] . "<br>name: " . $filename . "<br>folder with file name: " . $target_file . "<br>";
+echo "<P>Error on file loading: " . $file["error"] . "</P>";
+}
 
 $id = 0;
 $SQLresult = mysqli_query($link, 'select id from foto');
@@ -32,7 +32,7 @@ while($result = mysqli_fetch_array($SQLresult, MYSQLI_NUM))
 $id = $id+1;
 $two = mysqli_real_escape_string($link, $_POST['apartment']);
 $SQLquery = 'insert into foto values(' . $id . ', "' . $filename . '", ' . $two . ')';
-move_uploaded_file($filename, $target_file);
+
 if (move_uploaded_file($file['tmp_name'], $target_file))
 {
 	if (mysqli_query($link, $SQLquery))
