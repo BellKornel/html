2ba9e8ec67c3ada123babf2271819f52cec0c99f
Bell <46092446+BellKornel@users.noreply.github.com>
2018-12-29 16:14:56 +0900

    Update foto_action.php

--- foto_action.php
+++ foto_action.php
@@ -3,24 +3,24 @@ $link = mysqli_connect('0.0.0.0', 'anton', '', 'DB')
 	or die('Error: ' . mysqli_connect_error());
 
 $file = $_FILES['photo'];
-$fliename = date('Y_m_d_H_i_s');
+$filename = date('Y_m_d_H_i_s');
 $target_dir = "file/";
 $target_file = $target_dir . $filename;
 
-if ( !file_exists($target_dir) ) {
-     mkdir ($target_dir, 0744);
-echo "dir doesn't exist, trying to create dir!";
-}
+//if ( !file_exists($target_dir) ) {
+//     mkdir ($target_dir, 0744);
+//echo "dir doesn't exist, trying to create dir!";
+//}
 
-if (move_uploaded_file($filename, $target_file))
-{
-echo "<P>Photo uploaded succesfully</P>";
-}
-else
-{
-echo "tmp_name: " . $file["tmp_name"] . "<br>name: " . $filename . "<br>folder with file name: " . $target_file . "<br>";
-echo "<P>Error on file loading: " . $file["error"] . "</P>";
-}
+//if (move_uploaded_file($filename, $target_file))
+//{
+//echo "<P>Photo uploaded succesfully</P>";
+//}
+//else
+//{
+//echo "tmp_name: " . $file["tmp_name"] . "<br>name: " . $filename . "<br>folder with file name: " . $target_file . "<br>";
+//echo "<P>Error on file loading: " . $file["error"] . "</P>";
+/}
 
 $id = 0;
 $SQLresult = mysqli_query($link, 'select id from foto');
