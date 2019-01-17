e1768804c37c1c4cfb57c990e728614a73264a27
Bell <46092446+BellKornel@users.noreply.github.com>
2019-01-17 13:03:02 +0900

    Update action.php

--- matan/action.php
+++ matan/action.php
@@ -1,5 +1,5 @@
 <?php
   $link = mysqli_connect('0.0.0.0', 'anton', '', 'DB') or die('Error: ' . mysqli_connect_error());
   $one = mysqli_real_escape_string($link, $_POST['matan']);
-  printf(%one);
+  echo $one;
 ?>
