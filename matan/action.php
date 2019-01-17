d9b3d16fd1b4dd6e43b9bf3887b6d351a6879683
Bell <46092446+BellKornel@users.noreply.github.com>
2019-01-17 13:16:29 +0900

    Update action.php

--- matan/action.php
+++ matan/action.php
@@ -1,20 +1,10 @@
-<html>
-    <head>
-        <title>Matan</title>
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    </head>
-    <body>
-        <center>
-            <?php
-                $link = mysqli_connect('0.0.0.0', 'anton', '', 'DB') or die('Error: ' . mysqli_connect_error());
-                $one = mysqli_real_escape_string($link, $_POST['matan']);
-                $SQLquery = 'select id, foto from matan where id == '.$one;
-                $SQLresult = mysqli_query($link,$SQLquery);
-                while ($result = mysqli_fetch_array($SQLresult,MYSQLI_NUM))
-		{
-			printf('<TD><a href="%s" target="_blank"><img src="%s"></a></TD>', $result[1], $result[1]);
-		}
-            ?>
-        </center>
-    </body>
-</html>
+<?php
+$link = mysqli_connect('0.0.0.0', 'anton', '', 'DB') or die('Error: ' . mysqli_connect_error());
+$one = mysqli_real_escape_string($link, $_POST['matan']);
+$SQLquery = 'select id, foto from matan where id == '.$one;
+$SQLresult = mysqli_query($link,$SQLquery);
+while ($result = mysqli_fetch_array($SQLresult,MYSQLI_NUM))
+{
+	printf('<TD><a href="%s" target="_blank"><img src="%s"></a></TD>', $result[1], $result[1]);
+}
+?>
