f6bc98f7dbab160e07fa127a404f976e88352b47
Bell <46092446+BellKornel@users.noreply.github.com>
2019-01-17 13:12:24 +0900

    Update action.php

--- matan/action.php
+++ matan/action.php
@@ -8,7 +8,7 @@
             <?php
                 $link = mysqli_connect('0.0.0.0', 'anton', '', 'DB') or die('Error: ' . mysqli_connect_error());
                 $one = mysqli_real_escape_string($link, $_POST['matan']);
-                $SQLquery = 'select id, foto from matan where id == %s', $one;
+                $SQLquery = 'select id, foto from matan where id == '.$one;
                 $SQLresult = mysqli_query($link,$SQLquery);
                 while ($result = mysqli_fetch_array($SQLresult,MYSQLI_NUM))
 		{
