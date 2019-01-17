b3885dbaa95e48b0b1665e0a7e10fc9ddd63207b
Bell <46092446+BellKornel@users.noreply.github.com>
2019-01-17 13:18:05 +0900

    Update action.php

--- matan/action.php
+++ matan/action.php
@@ -1,10 +1,10 @@
 <?php
-$link = mysqli_connect('0.0.0.0', 'anton', '', 'DB') or die('Error: ' . mysqli_connect_error());
-$one = mysqli_real_escape_string($link, $_POST['matan']);
-$SQLquery = 'select id, foto from matan where id == '.$one;
-$SQLresult = mysqli_query($link,$SQLquery);
-while ($result = mysqli_fetch_array($SQLresult,MYSQLI_NUM))
-{
-	printf('<TD><a href="%s" target="_blank"><img src="%s"></a></TD>', $result[1], $result[1]);
-}
+	$link = mysqli_connect('0.0.0.0', 'anton', '', 'DB') or die('Error: ' . mysqli_connect_error());
+	$one = mysqli_real_escape_string($link, $_POST['matan']);
+	$SQLquery = 'select id, foto from matan where id == '.$one;
+	$SQLresult = mysqli_query($link,$SQLquery);
+	while ($result = mysqli_fetch_array($SQLresult,MYSQLI_NUM))
+	{
+		echo '<a href="'.$result[1].'" target="_blank"><img src="'.$result[1].'"></a>';
+	}
 ?>
