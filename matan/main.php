8617ff5ce47d15202105a0db3737bab7b777454f
Bell <46092446+BellKornel@users.noreply.github.com>
2019-01-17 12:59:15 +0900

    Update main.php

--- matan/main.php
+++ matan/main.php
@@ -8,20 +8,24 @@
 		<P style="color:black; font-size:18">
 			БЛОК 2
 		</P>
-		matan: 
-			<select name="matan">
-				<?php 
-					$link = mysqli_connect('0.0.0.0', 'anton', '', 'DB') or die('Error: ' . mysqli_connect_error());
-					$SQLquery = 'SELECT * FROM matan';
-					$SQLresult = mysqli_query($link,$SQLquery);
-					while ($result = mysqli_fetch_array($SQLresult,MYSQLI_NUM))
-					{
-						printf('<option value=%d>%s</option>',$result[0],$result[1]);
-					}
-					mysqli_free_result($SQLresult);
-					mysqli_close($link);
-				?>
-			</select>
+		<form action="action.php" method="post">
+			matan: 
+				<select name="matan">
+					<?php 
+						$link = mysqli_connect('0.0.0.0', 'anton', '', 'DB')
+							or die('Error: ' . mysqli_connect_error());
+						$SQLquery = 'SELECT * FROM matan';
+						$SQLresult = mysqli_query($link,$SQLquery);
+						while ($result = mysqli_fetch_array($SQLresult,MYSQLI_NUM))
+						{
+							printf('<option value=%d>%s</option>',$result[0],$result[1]);
+						}
+						mysqli_free_result($SQLresult);
+						mysqli_close($link);
+					?>
+				</select>
+			<input type="submit" value="Open">
+		</form>
 		<a href="../index.html"> <P>GO BACK</P></a>
 	</center>
  </body>
